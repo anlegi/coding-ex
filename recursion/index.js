beb0b2d3968c2939d144aa83110b288321e3f1b1
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-04-19 15:41:52 +0200

    Recursion until Q4

--- recursion/index.js
+++ recursion/index.js
@@ -16,3 +16,52 @@ function sumRange(n, sum = 0) {
 //     return 6
 //   return 6
 // return 6
+
+function secondSumRange(n) {
+  if (n == 1) {
+    return 1
+  }
+  return n + sumRange(n - 1)
+}
+
+// secondSumRange(3)
+//  secondSumRange(2)
+//    secondSumRange(1)
+//    return 1
+//  return 2 + 1
+// return 3 + 3
+
+
+function power(n, e) {
+  if (e == 0) {
+    return 1
+  }
+  return n * power(n, e-1)
+}
+
+// power(2, 3)
+//  power(2, 2)
+//    power(2, 1)
+//      power(2, 0)
+//      return (1)
+//    return 2 * (1)
+//  return 2 * (2 * 1)
+// return 2 * (2 * 2 * 1)
+
+
+function factorial(n) {
+  if (n == 1 || n == 0 ) {
+    return 1
+  }
+  return n * factorial(n-1)
+}
+
+
+function all(arr, fn) {
+  console.log(arr)
+  if (arr.length === 0) {
+    return true
+  }
+
+  return fn(arr.shift()) && all(arr, fn)
+}
