821d5fe83af1ec0d6a91b14c0158cbe5131f1775
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-06-10 10:15:23 +0200

    Update persistence function

--- persistent-big/index.js
+++ persistent-big/index.js
@@ -1,4 +1,14 @@
 function persistence(num) {
-  let splitNumber = num.split("")
-  
+  let count = 0
+  while (num > 9) {
+    let splitNumber = num.toString().split("").map(Number)
+    let product = 1
+
+    for (let i = 0; i < splitNumber.length; i++) {
+      product *= splitNumber[i]
+    }
+    num = product
+    count++
+  }
+  return count
 }
