d093ee09b053f1fe29fe28566c8835618da9fb1b
Anhnam LeGiang anhnamlegiang@gmail.com <anhnamlegiang@gmail.com>
2024-07-05 20:38:38 +0200

    Add funciton

--- reverse-list/index.js
+++ reverse-list/index.js
@@ -1,3 +1,7 @@
-function reverseList(list) {
-
+function reverseList(arr) {
+    let reversedList = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversedList.push(arr[i]);
+    }
+    return reversedList;
 }
